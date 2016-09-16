082d766b2382d4a62acec10536c0a591b985a2e0
Paul Silva <paul.silva@yachaytech.edu.ec>
2016-09-16 18:04:51 -0500

    Text Field and Text View (with placeholder) implemented in RateViewController

--- PruebaMap/RateViewController.swift
+++ PruebaMap/RateViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 import SwiftyJSON
 import Cosmos
+import Alamofire
 
-class RateViewController: UIViewController { //, UITableViewDataSource, UITableViewDelegate {
+class RateViewController: UIViewController, UITextViewDelegate {
     
-    // MARK: -
-    // MARK: Properties
+    // MARK: - Properties
     
-//    var selectedLocation: LocationModel = LocationModel()
-//    @IBOutlet weak var listTableView: UITableView!
+    @IBOutlet weak var commentTextView: UITextView!
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var rateController: CosmosView!
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    let placeholderText = "Escribe tu comentario o sugerencia."
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,48 +29,95 @@ class RateViewController: UIViewController { //, UITableViewDataSource, UITableV
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
         
+        nameTextField.layer.borderWidth = 1
+        nameTextField.layer.borderColor = UIColor.blackColor().CGColor
+        nameTextField.layer.cornerRadius = 5
+        
+        commentTextView.editable = true
+        commentTextView.selectable = true
+        commentTextView.font = UIFont.systemFontOfSize(17)
+        commentTextView.userInteractionEnabled = true
+        commentTextView.multipleTouchEnabled = true
+        commentTextView.backgroundColor = UIColor.clearColor()
+        commentTextView.layer.cornerRadius = 5
+        commentTextView.layer.borderWidth = 1
+        commentTextView.layer.borderColor = UIColor.blackColor().CGColor
+        applyPlaceholderStyle(commentTextView, placeholderText: placeholderText)
+        commentTextView.delegate = self
+        
         rateController.rating = 0
         rateController.settings.fillMode = .Half
+        rateController.didFinishTouchingCosmos = {
+            rating in
+            print(rating)
+        }
         rateController.settings.minTouchRating = 0
         rateController.settings.updateOnTouch = true
-        
-//        // set delegates and initialize homeModel
-//        
-//        self.listTableView.delegate = self
-//        self.listTableView.dataSource = self
-//        self.listTableView.reloadData()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func sendButton(sender: UIButton) {
         
     }
     
-//    func itemsDownloaded(items: JSON) {
-//        feedItems = items
-//        self.listTableView.reloadData()
-//    }
+    // MARK: - Delegate Methods
     
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // Return the number of feed items
-////        return feedItems.count
-//        return 11
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        // Retrieve cell
-//        let cellIdentifier: String = "BasicCell"
-//        let myCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)!
-////        // Get the location to be shown
-////        let item = feedItems[indexPath.row]
-////        // Get references to labels of cell
-////        if item["paradas"].isEmpty {
-////            myCell.textLabel!.text = "()"
-////        } else {
-////            var routeArray: String = ""
-////            for j in 0..<item["paradas"].count {
-////                routeArray += ("(\(item["paradas",j])) ")
-////            }
-////            myCell.textLabel!.text = String(routeArray)
-////        }
-////        myCell.textLabel!.text = String(item["paradas"])
-//        return myCell
-//    }
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+        if textView == commentTextView && textView.text == placeholderText {
+            // move cursor to start
+            moveCursorToStart(textView)
+        }
+        return true
+    }
+    
+    func textViewDidChangeSelection(textView: UITextView) {
+        if textView == commentTextView && textView.text == placeholderText {
+            moveCursorToStart(textView)
+        }
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        // remove the placeholder text when users start typing
+        let newLength = textView.text.utf16.count + text.utf16.count - range.length
+        if newLength > 0 {
+            if textView == commentTextView && textView.text == placeholderText {
+                if text.utf16.count == 0 {
+                    return false
+                }
+                applyNonPlaceholderStyle(textView)
+                textView.text = ""
+            }
+            return true
+        } else {
+            applyPlaceholderStyle(textView, placeholderText: placeholderText)
+            moveCursorToStart(textView)
+            return false
+        }
+    }
+    
+    func saveRating() {
+        
+    }
+    
+    // MARK: - Helper Functions
+    
+    func applyPlaceholderStyle(textView: UITextView, placeholderText: String) {
+        // make it look (initially) like a placeholder
+        textView.textColor = UIColor.lightGrayColor()
+        textView.text = placeholderText
+    }
+    
+    func applyNonPlaceholderStyle(textView: UITextView) {
+        // make text look normal again
+        textView.textColor = UIColor.darkTextColor()
+        textView.alpha = 1.0
+    }
+    
+    func moveCursorToStart(textView: UITextView) {
+        dispatch_async(dispatch_get_main_queue(), {
+            textView.selectedRange = NSMakeRange(0, 0)
+        })
+    }
     
 }
